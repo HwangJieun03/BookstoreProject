6863df78e725e1b835d5137675dbeaee63f85299
jieun <jieunh0311@gmail.com>
2025-03-13 14:27:06 +0900

    도서 목록 조회 페이징 구현

--- routes/books.js
+++ routes/books.js
@@ -1,22 +1,13 @@
 const express = require("express");
 const router = express.Router();
-
+const { allBooks, bookDetail } = require("../controller/BookController");
 router.use(express.json());
 
-// 도서 전체 조회
-router.get("/", (req, res) => {
-  res.json("도서 전체 조회");
-});
+
+// 도서 전체 조회, 카테고리별 도서 목록 조회 
+router.get("/", allBooks);
 
 // 도서 개별 조회
-router.get("/:id", (req, res) => {
-  res.json("도서 개별 조회");
-});
-
-// 카테고리별 도서 목록 조회
-router.get("/ ", (req, res) => {
-  req.query.categoryId;
-  res.json("카테고리별 도서 목록 조회");
-});
+router.get("/:id", bookDetail);
 
 module.exports = router;
